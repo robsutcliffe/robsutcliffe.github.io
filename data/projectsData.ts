c4c1235c683a9ea232b0ea3d4a6f0196e50c49b0
robsutcliffe <rob@firefields.com>
2023-12-21 12:38:57 +0100

    empty project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,10 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'first project',
+    description: `First project description`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://robsutcliffe.github.io',
+  },
+]
 
 export default projectsData
