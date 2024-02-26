a8646e564a0a3e951d332187ff511e549967632e
robsutcliffe <rob@firefields.com>
2024-02-26 21:36:16 +0100

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,21 @@
 const projectsData = [
   {
-    title: 'first project',
-    description: `First project description`,
+    title: 'Economist Intelligence 75th Birthday',
+    description: `A microsite for The Economist Intelligence 75th anniversary. I worked directly with the designer, strategist, and client to design and develop a unique one-off site.`,
     imgSrc: '/static/images/logo.png',
-    href: 'https://robsutcliffe.github.io',
+    href: 'https://www.youtube.com/watch?v=-hYjk9hpWWo',
+  },
+  {
+    title: 'Language Learning with Video App',
+    description: `A Strapi and Next.js application with scraping, fuzzy search, and a custom video player. Its features include JWT OAuth, fuzzy search, and a daily cron job to mirror existing databases and images.`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://youtu.be/2vUus1VzEyE',
+  },
+  {
+    title: 'Socket.IO Real-time Game',
+    description: `An application for students to watch and interact with films in real time and for teachers to control the film and see results at the end. The application heavily uses WebSockets and is animated with Framer Motion.`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://youtu.be/c3r1glqMMsU',
   },
 ]
 
