d10f03d13fd4084460c00deab28914827126788c
robsutcliffe <rob@firefields.com>
2024-06-28 23:58:30 +0100

    webp images

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,31 +2,31 @@ const projectsData = [
   {
     title: 'Economist Intelligence 75th Birthday',
     description: `A microsite for The Economist Intelligence 75th anniversary. I worked directly with the designer, strategist, and client to design and develop a unique one-off site.`,
-    imgSrc: '/static/images/projects/economist.png',
+    imgSrc: '/static/images/projects/economist.webp',
     href: 'https://www.youtube.com/watch?v=-hYjk9hpWWo',
   },
   {
     title: 'Language Learning with Video App',
     description: `A Strapi and Next.js application with scraping, fuzzy search, and a custom video player. Its features include JWT OAuth, fuzzy search, and a daily cron job to mirror existing databases and images.`,
-    imgSrc: '/static/images/projects/language_learning.png',
+    imgSrc: '/static/images/projects/language_learning.webp',
     href: 'https://youtu.be/2vUus1VzEyE',
   },
   {
     title: 'Socket.IO Real-time Game',
     description: `An application for students to watch and interact with films in real time and for teachers to control the film and see results at the end. The application heavily uses WebSockets and is animated with Framer Motion.`,
-    imgSrc: '/static/images/projects/game.png',
+    imgSrc: '/static/images/projects/game.webp',
     href: 'https://youtu.be/c3r1glqMMsU',
   },
   {
     title: 'News Feed Scrapping and Summarising',
     description: `Full stack next.js application with a daily cron job to scrape, summarise and tag web development blog posts. Uses fuzzy search directly in mysql for fast searching.`,
-    imgSrc: '/static/images/projects/fastfeed.png',
+    imgSrc: '/static/images/projects/fastfeed.webp',
     href: 'https://ffeed.vercel.app/',
   },
   {
     title: 'AI Treemap Generator',
     description: `Use various AI models to categories open ended survey responses and check with a the user at various stages if the categories are useful and accurate.`,
-    imgSrc: '/static/images/projects/survey.png',
+    imgSrc: '/static/images/projects/survey.webp',
     href: 'https://youtu.be/aAZPw7p2GbI',
   },
 ]
