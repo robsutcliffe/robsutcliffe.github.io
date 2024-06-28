635dc746521ad281418d4ffd2ee35e494455b750
robsutcliffe <rob@firefields.com>
2024-06-28 10:30:42 +0100

    initial projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,20 +2,26 @@ const projectsData = [
   {
     title: 'Economist Intelligence 75th Birthday',
     description: `A microsite for The Economist Intelligence 75th anniversary. I worked directly with the designer, strategist, and client to design and develop a unique one-off site.`,
-    imgSrc: '/static/images/logo.png',
+    imgSrc: '/static/images/projects/economist.png',
     href: 'https://www.youtube.com/watch?v=-hYjk9hpWWo',
   },
   {
     title: 'Language Learning with Video App',
     description: `A Strapi and Next.js application with scraping, fuzzy search, and a custom video player. Its features include JWT OAuth, fuzzy search, and a daily cron job to mirror existing databases and images.`,
-    imgSrc: '/static/images/logo.png',
+    imgSrc: '/static/images/projects/language_learning.png',
     href: 'https://youtu.be/2vUus1VzEyE',
   },
   {
     title: 'Socket.IO Real-time Game',
     description: `An application for students to watch and interact with films in real time and for teachers to control the film and see results at the end. The application heavily uses WebSockets and is animated with Framer Motion.`,
-    imgSrc: '/static/images/logo.png',
+    imgSrc: '/static/images/projects/game.png',
     href: 'https://youtu.be/c3r1glqMMsU',
+  },
+  {
+    title: 'News Feed Scrapping and Summarising',
+    description: `Full stack next.js application with a daily cron job to scrape, summarise and tag web development blog posts. Uses fuzzy search directly in mysql for fast searching.`,
+    imgSrc: '/static/images/projects/fastfeed.png',
+    href: 'https://ffeed.vercel.app/',
   },
 ]
 
