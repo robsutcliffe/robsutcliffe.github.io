5e01e5286cd421481c19d5c5066bedbd7b639bf2
robsutcliffe <rob@firefields.com>
2026-03-22 12:28:23 +0700

    mobile view height

--- components/Border.tsx
+++ components/Border.tsx
@@ -9,14 +9,11 @@ export default function Border() {
       <Logo />
       <SearchBar />
       {/*<Menu />*/}
-      <div className="pointer-events-none fixed inset-0 z-30">
-        <div className="absolute inset-0 z-30">
-          <div className="absolute top-0 right-0 left-0 z-20 h-2 w-full bg-white md:h-4 lg:h-6" />
-          <div className="absolute right-0 bottom-0 left-0 z-20 h-2 w-full bg-white md:h-4 lg:h-6" />
-          <div className="absolute top-0 bottom-0 left-0 z-20 h-full w-2 bg-white md:w-4 lg:w-6" />
-          <div className="absolute top-0 right-0 bottom-0 z-20 h-full w-2 bg-white md:w-4 lg:w-6" />
-        </div>
-      </div>
+
+      <div className="fixed top-0 right-0 left-0 z-30 h-2 w-full bg-white md:h-4 lg:h-6" />
+      <div className="fixed right-0 bottom-0 left-0 z-30 h-2 w-full bg-white md:h-4 lg:h-6" />
+      <div className="fixed top-0 bottom-0 left-0 z-30 h-svh w-2 bg-white md:w-4 lg:w-6" />
+      <div className="fixed top-0 right-0 bottom-0 z-30 h-svh w-2 bg-white md:w-4 lg:w-6" />
     </>
   )
 }
