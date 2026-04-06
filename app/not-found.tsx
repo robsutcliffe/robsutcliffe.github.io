f50838ec92a705abc3e7b635feb641b9b4ea58ee
robsutcliffe <rob@firefields.com>
2026-04-06 12:01:43 +0200

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,38 @@
 import Link from '@/components/Link'
+import DotBackground from '@/components/DotBackground'
+import Button from '@/components/Button'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl leading-9 font-extrabold tracking-tighter text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
+    <div className="relative flex min-h-[calc(100vh-80px)] flex-col items-center justify-center border-b border-blue-100/50 bg-blue-800 text-center">
+      <div className="z-20 pt-24">
+        <div className="pb-8">
+          <h1 className="text-6xl leading-9 tracking-tighter text-yellow-200 md:px-6 md:text-8xl md:leading-14">
+            404
+          </h1>
+        </div>
+        <div className="max-w-md">
+          <p className="mb-0 text-xl leading-normal font-extrabold text-white md:text-2xl">
+            Sorry we couldn't find this page.
+          </p>
+          <p className="mb-8 text-white/80">
+            Maybe you can find something else that interests you from the home page or the menu at
+            the top right.
+          </p>
+          <div className="mx-auto w-fit">
+            <Button
+              href="/"
+              text="Back to homepage"
+              extraClasses="!text-blue-800 !bg-white"
+              noPadding={true}
+              outline={true}
+              lineOpacity={0.2}
+              noHeight={true}
+            />
+          </div>
+        </div>
       </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
-          Sorry we couldn't find this page.
-        </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-        <Link
-          href="/"
-          className="focus-ring inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm leading-5 font-medium text-white shadow transition-colors duration-150 hover:bg-blue-700"
-        >
-          Back to homepage
-        </Link>
-      </div>
+      <DotBackground fill="#ffffff" opacity="0.1" />
     </div>
   )
 }
