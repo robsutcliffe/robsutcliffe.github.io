e37fef683f4a3e61eac7870000bd2479de5afa0e
robsutcliffe <rob@firefields.com>
2026-05-26 10:53:54 +0200

    add view services button

--- components/DotsHero.tsx
+++ components/DotsHero.tsx
@@ -37,6 +37,16 @@ export default function Hero() {
           <div className="mx-auto flex w-full flex-col items-center justify-center gap-4 sm:flex-row">
             <div>
               <Button
+                href="/services"
+                text="View services"
+                noPadding={true}
+                outline={true}
+                lineOpacity={0.2}
+                noHeight={true}
+              />
+            </div>
+            <div>
+              <Button
                 href="/contact"
                 text="Book a call"
                 noPadding={true}
